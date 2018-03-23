67c7ab2ffff3cfdd73e0a1814b03cc776455cddd
danieleferemo <efrdan001@myuct.ac.za>
2018-03-23 20:05:02 +0200

    Initial Commit

---
+++ WeatherForecaster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeatherForecaster
+//
+//  Created by Daniel Eferemo on 3/23/18.
+//  Copyright © 2018 danieleferemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
